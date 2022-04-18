219d9fecd67ffb4c6da3b0808ecb67db47968f7d
Leticia Dias <contatoleticiadia@gmail.com>
2022-04-18 19:21:27 -0300

    feat: routes file added

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index', {page: 'editor'});
+});
+
+module.exports = router;
